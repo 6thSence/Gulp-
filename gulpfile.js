a63a7337f8df7d6bfcacbda4f9c34b348d591420
6thSence <d.pushkarskaya.pr@gmail.com>
2016-04-26 00:15:16 +0600

    build

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,18 @@
 const gulp = require('gulp');
 
-gulp.task('default', ['hello']);
+gulp.task('default', ['html', 'styles', 'scripts']);
 
-gulp.task('hello', function() {
-	console.log('Hello!');
+gulp.task('styles', function() {
+	return gulp.src('src/**/*.css')
+		.pipe(gulp.dest('build/styles'));
+});
+
+gulp.task('html', function() {
+	return gulp.src('src/**/*.html')
+		.pipe(gulp.dest('build/'));
+});
+
+gulp.task('scripts', function() {
+	return gulp.src('src/**/*.js')
+		.pipe(gulp.dest('build/js'));
 });
