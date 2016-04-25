12a80ee91aca9734f181ea1a7db4402e0322ba0c
6thSence <d.pushkarskaya.pr@gmail.com>
2016-04-26 01:13:35 +0600

    styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,27 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const concat = require('gulp-concat');
+const cssnano = require('gulp-cssnano');
+const rename = require('gulp-rename');
+const compass = require('gulp-compass');
 
 gulp.task('default', ['html', 'styles', 'scripts']);
 
 gulp.task('styles', function() {
-	return gulp.src('src/**/*.css')
+	return gulp.src('src/styles/{main,about}.scss')
+		//.pipe(sass())
+		.pipe(compass({
+               config_file: './config.rb',
+               css: 'stylesheets',
+               sass: 'sass'
+             }))
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions']
+		}))
+		.pipe(concat('styles.css'))
+		.pipe(cssnano())
+		.pipe(rename('build.css'))
 		.pipe(gulp.dest('build/styles'));
 });
 
